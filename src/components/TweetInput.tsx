d6448ee1dc9acb34a605d1d7441d88283e1e78da
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-09-19 00:20:25 +0900

    tweet form

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -5,7 +5,7 @@ import { selectUser } from "../features/userSlice";
 import { storage, db, auth } from "../firebase";
 import { Avatar, Button, IconButton } from "@material-ui/core";
 import firebase from "firebase/app";
-import AddPhotoIcon from "@material-ui/icons/AddPhoto";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
 
 export const TweetInput = () => {
   const user = useSelector(selectUser);
@@ -62,15 +62,52 @@ export const TweetInput = () => {
     setTweetMsg("");
   };
   return (
-    <div>
-      <Avatar
-        className={styles.tweet_avatr}
-        src={user.photoUrl}
-        onClick={async () => {
-          await auth.signOut();
-        }}
-      />
-    </div>
+    <>
+      <form onSubmit={sendTweet}>
+        <div className={styles.tweet_form}>
+          <Avatar
+            className={styles.tweet_avatr}
+            src={user.photoUrl}
+            onClick={async () => {
+              await auth.signOut();
+            }}
+          />
+          <input
+            className={styles.tweet_input}
+            placeholder="What't happening?"
+            type="text"
+            autoFocus
+            value={tweetMsg}
+            onChange={(e) => setTweetMsg(e.target.value)}
+          />
+
+          <IconButton>
+            <label>
+              <AddAPhotoIcon
+                className={
+                  tweetImage ? styles.tweet_addIconLoaded : styles.tweet_addIcon
+                }
+              />
+              <input
+                className={styles.tweet_hiddenIcon}
+                type="file"
+                onChange={onChangeImageHandler}
+              />
+            </label>
+          </IconButton>
+        </div>
+
+        <Button
+          type="submit"
+          disabled={!tweetMsg}
+          className={
+            tweetMsg ? styles.tweet_sendBtn : styles.tweet_sendDisableBtn
+          }
+        >
+          Tweet
+        </Button>
+      </form>
+    </>
   );
 };
 
