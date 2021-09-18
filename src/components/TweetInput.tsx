6e4a7e9ccbac5a14b2e001a72e1fbd3e89b6840c
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-09-18 23:38:02 +0900

    add icon

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import styles from "./TweetInput.module.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { auth } from "../firebase";
+import { Avatar } from "@material-ui/core";
 
 export const TweetInput = () => {
+  const user = useSelector(selectUser);
   return (
     <div>
-      
+      <Avatar
+        className={styles.tweet_avatr}
+        src={user.photoUrl}
+        onClick={async () => {
+          await auth.signOut();
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default TweetInput;
