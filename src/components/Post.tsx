ed25e1377e0b92dff2f3a0d9fecc6e4f9a0e49a0
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-09-19 00:49:22 +0900

    create Post.tsx

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import styles from "./Post.module.css";
+import { db } from "../firebase";
+import firebase from "firebase/app";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import MessageIcon from "@material-ui/icons/Message";
+import SendIcon from "@material-ui/icons/Send";
+
+interface PROPS {
+  postId: string;
+  avatar: string;
+  image: string;
+  text: string;
+  timestamp: any;
+  username: string;
+}
+
+interface COMMENT {
+  id: string;
+  avatar: string;
+  text: string;
+  timestamp: any;
+  username: string;
+}
 
 export const Post = () => {
   return (
