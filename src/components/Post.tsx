486412c75a135f498f2c5ef66d9a5ec4da07d341
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-09-19 01:44:26 +0900

    comment feed

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -26,9 +26,52 @@ interface COMMENT {
   username: string;
 }
 
+const useStyles = makeStyles((theme) => ({
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    marginRight: theme.spacing(1),
+  },
+}));
+
 const Post: React.FC<PROPS> = (props) => {
-  const user =useSelector(selectUser);
+  const classes = useStyles();
+  const user = useSelector(selectUser);
+  const [openComments, setOpenComments] = useState(false);
   const [comment, setComment] = useState("");
+  const [comments, setComments] = useState<COMMENT[]>([
+    {
+      id: "",
+      avatar: "",
+      text: "",
+      username: "",
+      timestamp: null,
+    },
+  ]);
+
+  useEffect(() => {
+    const unSub = db
+      .collection("posts")
+      .doc(props.postId)
+      .collection("comments")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setComments(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            text: doc.data().text,
+            username: doc.data().username,
+            timestamp: doc.data().timestamp,
+          }))
+        );
+      });
+
+    return () => {
+      unSub();
+    };
+  }, [props.postId]);
+
   const newComment = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     db.collection("posts").doc(props.postId).collection("comments").add({
@@ -39,7 +82,6 @@ const Post: React.FC<PROPS> = (props) => {
     });
     setComment("");
   };
-
   return (
     <div className={styles.post}>
       <div className={styles.post_avatar}>
@@ -58,12 +100,33 @@ const Post: React.FC<PROPS> = (props) => {
           <div className={styles.post_tweet}>
             <p>{props.text}</p>
           </div>
-          {props.image && (
-            <div className={styles.post_tweetImage}>
-              <img src={props.image} alt="tweet" />
-            </div>
-          )}
-          <form onSubmit={newComment}>
+        </div>
+        {props.image && (
+          <div className={styles.post_tweetImage}>
+            <img src={props.image} alt="tweet" />
+          </div>
+        )}
+
+        <MessageIcon
+          className={styles.post_commentIcon}
+          onClick={() => setOpenComments(!openComments)}
+        />
+
+        {openComments && (
+          <>
+            {comments.map((com) => (
+              <div key={com.id} className={styles.post_comment}>
+                <Avatar src={com.avatar} className={classes.small} />
+
+                <span className={styles.post_commentUser}>@{com.username}</span>
+                <span className={styles.post_commentText}>{com.text} </span>
+                <span className={styles.post_headerTime}>
+                  {new Date(com.timestamp?.toDate()).toLocaleString()}
+                </span>
+              </div>
+            ))}
+
+            <form onSubmit={newComment}>
               <div className={styles.post_form}>
                 <input
                   className={styles.post_input}
@@ -85,11 +148,12 @@ const Post: React.FC<PROPS> = (props) => {
                 </button>
               </div>
             </form>
-        </div>
+          </>
+        )}
       </div>
     </div>
-  )
-
-}
+  );
+};
 
 export default Post;
+
